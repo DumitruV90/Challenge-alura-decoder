27b2b434dc10ddeeffeb1ffa4a7f482eaeea32d9
DumitruV90 <vasilescu288@gmail.com>
2022-01-27 20:40:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+Este repositorio hace parte del primer desafío del programa Oracle Next Education (ONE) desarrollado por Alura Latam. 
+
+El desafío consiste en desarrollar una aplicación web para encriptar texto. 
+
+Las "llaves" de encriptación son las siguientes:
+
+La letra "e" es convertida en "enter"
+La letra "i" es convertida en "imes"
+La letra "a" es convertida en "ai"
+La letra "o" es convertida en "ober"`
+La letra "u" es convertida en "ufat"
+
+**Requisitos:**
+- Debe funcionar solo con letras minúsculas.
+- No deben ser utilizados letras con acentos ni caracteres especiales.
+- Debe ser posible convertir una palabra a la versión encriptada y también devolver una palabra encriptada a su versión original. 
+
+Por ejemplo:
+"gato" => "gaitober"
+"gaitober" => "gato"
+
+- La página debe tener campos para la inserción del texto que será encriptado o desencriptado, y el usuario debe poder escoger entre las dos opciones.
+- El resultado debe ser mostrado en la pantalla.
+
+**Extras:**
+- Un botón que copie el texto encriptado/desencriptado para la sección de transferencia, o sea que tenga la misma funcionalidad del "ctrl+C" o de la opción "copiar" del menú de las aplicaciones.
+
---
+++ index.js
@@ -0,0 +1,103 @@
+
+/* Reglas de encriptación: 
+"e" es convertido para "enter" 
+"i" es convertido para "imes"
+"a" es convertido para "ai"
+"o" es convertido para "ober"
+"u" es convertido para "ufat"
+Solo letras minusculas
+No se permite acentuación de palabras 
+*/
+
+/* Reglas de desencriptación: 
+"enter" es convertido para "e" 
+"imes" es convertido para "i"
+"ai" es convertido para "a"
+"ober" es convertido para "o"
+"ufat" es convertido para "u"
+Solo letras minusculas
+No se permite acentuación de palabras   
+*/
+
+const inputText = document.getElementById("input-text");
+const encriptarBtn = document.getElementById("btn-encriptar");
+const desencriptarBtn = document.getElementById("btn-desencriptar");
+const copiarBtn = document.getElementById("btn-copiar");
+const limpiarBtn = document.getElementById("btn-limpiar");
+const toolTip = document.getElementById("myTooltip");
+
+
+
+encriptarBtn.addEventListener("click", function() {
+
+    //Retiro de acentos o caracteres especiales de la cadena de texto y conversión a minúsculas
+    let entradaTexto = inputText.value.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase(); 
+
+    //Encriptación de la cadena de texto
+    let textoFinal = "";
+    for (let vocal of entradaTexto) {
+        switch (vocal) {
+            case "e":
+                textoFinal += vocal.replace("e", "enter");
+                break;
+            case "i":
+                textoFinal += vocal.replace("i", "imes");
+                break;
+            case "a":
+                textoFinal += vocal.replace("a", "ai");
+                break;
+            case "o":
+                textoFinal += vocal.replace("o", "ober");
+                break;
+            case "u":
+                textoFinal += vocal.replace("u", "ufat");
+                break;
+            default:
+                textoFinal += vocal;
+        }
+    }
+    inputText.value = textoFinal;
+    
+});
+
+desencriptarBtn.addEventListener("click", function() {
+
+    // Creación del objeto que almacena los nuevos valores
+    let nuevosValores = {
+        enter: "e",
+        imes: "i",
+        ai: "a",
+        ober: "o",
+        ufat: "u"
+    }
+    
+    // Reemplazo con los nuevos valores del objeto
+    let textoNormal = inputText.value.replace(/enter|imes|ai|ober|ufat/gi, function(matched) {
+        return nuevosValores[matched]
+    });
+      
+    inputText.value = textoNormal; 
+
+});
+
+copiarBtn.addEventListener("click", function() {
+
+    // Selección del campo de texto
+    inputText.select();
+    inputText.setSelectionRange(0, 99999); // Para dispositivos móviles
+
+    // Selección del texto en el campo de texto
+    navigator.clipboard.writeText(inputText.value);
+
+    toolTip.innerHTML = "Copiado"
+
+});
+
+copiarBtn.addEventListener("mouseout", function() {
+    toolTip.innerHTML = "Copiar al portapapeles";
+});
+
+limpiarBtn.addEventListener("click", function() {
+    inputText.value = "";
+});
+
